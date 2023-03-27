e1b18bdcc03281d41ac133d60931c1f964b4d293
youjin <w2117@e-mirim.hs.kr>
2023-03-27 12:09:01 +0900

    Controller 정의

--- src/main/java/study/mvc/MyController.java
+++ src/main/java/study/mvc/MyController.java
@@ -1,4 +1,13 @@
 package study.mvc;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class MyController {
+    @GetMapping("/hello") //Get 요청
+    public String hello(){
+        return "hello"; //바디 영역의 응답 메시지로 보냄. text/plain (plain은 일반 타입)
+    }
 }
