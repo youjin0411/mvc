6d5674b9ba4f4a02cc5eb51cfcbf0b8d6b64a4a0
youjin <w2117@e-mirim.hs.kr>
2023-04-10 12:20:16 +0900

    MyController

--- src/main/java/study/mvc/MyController.java
+++ src/main/java/study/mvc/MyController.java
@@ -4,16 +4,18 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.util.ResourceUtils;
+import org.springframework.web.bind.annotation.*;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.util.ArrayList;
 import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.Objects;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api")
@@ -101,4 +103,80 @@ public class MyController {
         response.getWriter().write("{ \"data\": \"Hello\" }");
     }
 
+    @GetMapping("/echo-repeat")
+    public void echoRepeat(HttpServletRequest request, HttpServletResponse response)
+            throws IOException {
+        response.setStatus(200);
+        response.setHeader("Content-Type", "text/plain");
+        // X-Repeat-Count에 적힌 숫자 정보 가져오고 없으면 1로 초기화
+        int loopCount = Integer.parseInt(request.getHeader("X-Repeat-Count") == null
+                ? "1" : request.getHeader("X-Repeat-Count"));
+        // 쿼리 스트링 정보 가져와서
+        String query = request.getQueryString();
+        // 쿼리 스트링 나누고
+        String[] querySplit = query.split("&");
+        String result = "";
+        // 각 쿼리 스트링 정보들을 X-Repeat-Count만큼 반복해서 보여주기
+        for(String s : querySplit) {
+            for(int i=0;i<loopCount;i++) {
+                String[] tmp = s.split("=");
+                result += tmp[0] + "," + tmp[1] + "\n";
+            }
+        }
+        response.getWriter().write(result.trim());
+    }
+
+    @GetMapping("/dog-image")
+    public void dogImage(HttpServletResponse response) throws IOException {
+    // resources 폴더의 static 폴더에 이미지 있어야 함
+        File file = ResourceUtils.getFile("classpath:static/dog.jpg");
+    // 파일의 바이트 데이터 모두 읽어오기
+        byte[] bytes = Files.readAllBytes(file.toPath());
+        response.setStatus(200);
+    // 응답 메시지의 데이터가 JPEG 압축 이미지임을 설정
+        response.setHeader("Content-Type", "image/jpeg");
+    // 바이트 데이터 쓰기 (여기서는 텍스트 데이터를 전송하지 않기 떄문에 Writer 대신OutputStream을 이용)
+        // 바이트 배열 -> 바이너리 파일은 getOutputStream으로 바이트 배열을 받는다.
+        response.getOutputStream().write(bytes);
+    }
+
+    @GetMapping("/dog-image-file")
+    public void dogImageFile(HttpServletResponse response) throws IOException {
+        File file = ResourceUtils.getFile("classpath:static/dog.jpg");
+        byte[] bytes = Files.readAllBytes(file.toPath());
+        response.setStatus(200);
+// 임의의 바이너리 데이터임을 알려주는 MIME 타입 설정
+        response.setHeader("Content-Type", "application/octet-stream");
+// Content-Length는 자동으로 파일 크기만큼 설정해주지만 여기서는 그냥 넣었음
+// (Q) 만약 바이트 크기를 제대로 주지 않으면 어떻게 될까?)
+        // Content-Length는 주지 않으면 자동 설정됨. 굳이 손 되지 마라
+        response.setHeader("Content-Length", bytes.length + "");
+// 다운로드 될 파일 이름 설정
+        String filename = "dog.jpg";
+        response.setHeader("Content-Disposition", "attachment; filename=" +
+                filename);
+        response.getOutputStream().write(bytes);
+    }
+    //이곳은 원래 DB가 들어가야 할 자리
+    private ArrayList<String> wordList = new ArrayList<>();
+    // 위의 ArrayList에 단어를 추가하는 메서드
+    @PostMapping("/words")
+    public void addWord(HttpServletRequest request, HttpServletResponse response)
+            throws IOException {
+        String bodyString =
+                request.getReader().lines().collect(Collectors.joining(System.lineSeparator())); //body 데이터에 엔터 포함 모두 보내주는 것 lineSeparator
+        String[] words = bodyString.split("\n");
+        for(String w : words) wordList.add(w.trim());
+    // 201 CREATED 응답 코드 발생시키기
+        response.setStatus(201);
+    // 생성된 리소스를 확인할 수 있는 URL 알려주기 (Location 헤더 굳이 안 붙여도 기능적으로 차이는 없음)
+        response.setHeader("Location", "/words");
+    }
+    // 저장된 모든 단어 보여주기
+    @GetMapping("/words")
+    public void showWords(HttpServletResponse response) throws IOException {
+        String allWords = String.join(",", wordList);
+        response.setStatus(200);
+        response.getWriter().write(allWords);
+    }
 }
